d47f521b81d8278f4d42e598ca0013fcd10bf925
Joao Manieri <jotamanieri@gmail.com>
2024-02-23 11:10:01 -0300

    add all

---
+++ Barbearia/Populacao.sql
@@ -0,0 +1,467 @@
+-- Inserindo Clientes
+INSERT INTO barbearia.cliente
+(nome, email, genero, idlocalizacao)
+VALUES('Rodrigo Castro', 'rod300603@gmail.com', 'Masculino', 1);
+
+INSERT INTO barbearia.cliente
+(nome, email, genero, idlocalizacao)
+VALUES('Israel Goularte', 'israel@gmail.com', 'Masculino', 4);
+
+INSERT INTO barbearia.cliente
+(nome, email, genero, idlocalizacao)
+VALUES('Fulano', 'fulano@gmail.com', 'masculino', 9);
+
+INSERT INTO barbearia.cliente
+(nome, email, genero, idlocalizacao)
+VALUES('Fernando', 'fernando@gmail.com', 'masculino', 10);
+
+INSERT INTO barbearia.cliente
+(nome, email, genero, idlocalizacao)
+VALUES('Igor', 'igor@gmail.com', 'masculino', 11);
+
+INSERT INTO barbearia.cliente
+(nome, email, genero, idlocalizacao)
+VALUES('Leia', 'leila@gmail.com', 'feminino', 12);
+
+INSERT INTO barbearia.cliente
+(nome, email, genero, idlocalizacao)
+VALUES('Pedro', 'pedro@gmail.com', 'masculino', 13);
+
+INSERT INTO barbearia.cliente
+(nome, email, genero, idlocalizacao)
+VALUES('Naty', 'naty@gmail.com', 'feminino', 14);
+
+INSERT INTO barbearia.cliente
+(nome, email, genero, idlocalizacao)
+VALUES('Rebeca', 'rebeca@gmail.com', 'feminino', 15);
+
+INSERT INTO barbearia.cliente
+(nome, email, genero, idlocalizacao)
+VALUES('Lucas', 'lucas@gmail.com', 'masculino', 16);
+
+
+
+
+-- Inserindo localizações
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('38408-140', 'Brasil', 'MG', 'Uberlândia', 'Santa Monica', 'Rua Jose Paes de Almeida', '600', 'apt 302', 'Residencial');
+
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('38408-202', 'Brasil', 'MG', 'Uberlândia', 'Saraiva', 'Rua Andorinha', '213', 'Casa', 'Residencial');
+
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('01153 000', 'Brasil', 'SP', 'São Paulo', 'Centro', 'Rua das Flores', '85', 'Comércio', 'Comercial');
+
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('38400200', 'Brasil', 'MG', 'Uberlândia', 'Morumbi', 'Rua Eduardo Costa', '122', 'Casa', 'Residencial');
+
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('01153 300', 'Brasil', 'SP', 'Iraçatua', 'Jardim Europa', 'Rua das Couves', '555', 'Comércio', 'Comercial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('38408228', 'Brasil', 'MG', 'Uberlandia', 'Santa Monica', 'Rua da Cenoura', '123', 'Apto 305', 'Residencial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('38408450', 'Brasil', 'MG', 'Uberlandia', 'Granada', 'Avenida Joao Silva', '657', 'Casa', 'Residencial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13571015', 'Brasil', 'SP', 'Sao Carlos', 'Jockey Clube A', 'Rua Ray Wesley Herrick', '1500', 'casa 30', 'Residencial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13560646', 'Brasil', 'SP', 'Sao Carlos', 'Jardim Paulista', 'Rua Delfino martins', '64', 'casa 21', 'Residencial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13560789', 'Brasil', 'SP', 'Sao Carlos', 'Jardim do vape', 'Rua do carburas', '16', 'casa 98', 'Residencial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13560589', 'Brasil', 'SP', 'Sao Carlos', 'Jardim de bandis', 'Rua dos Crias', '69', 'casa 23', 'Residencial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('04771180', 'Brasil', 'SP', 'Sao Paulo', 'Interlagos', 'Avenida Clara Mantelli', '129', 'laguinho', 'Residencial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13571150', 'Brasil', 'SP', 'Sao Carlos', 'Cardinalli', 'Rua Irma Maria San felix', '68', 'casa de pedra', 'Residencial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('14021673', 'Brasil', 'SP', 'Ribeirao Preto', 'Subsetor 7', 'Rua jose tadeu sincos', '120', 'apto 114', 'Residencial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('14110000', 'Brasil', 'SP', 'Ribeirao Preto', 'Bonfim Paulista', 'Rua Carlos Rateb Cury', '100', 'casa 64', 'Residencial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13568525', 'Brasil', 'SP', 'Sao Carlos', 'Villa Nery', 'Rua Romeu Tortorelli', '78', 'casa amarela', 'Residencial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13561050', 'Brasil', 'SP', 'Sao Carlos', 'Jardim Paulista', 'Rua Padre Teixeira', '2670', 'apto 113', 'Residencial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('08471460', 'Brasil', 'SP', 'Sao Paulo', 'Cidade Tiradentes', 'Rua da Sorte', '18', 'Beco 12', 'Comercial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13560100', 'Brasil', 'SP', 'Sao Carlos', 'Centro', 'Avenida Sao Carlos', '2211', 'x', 'Comercial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13560654', 'Brasil', 'SP', 'Sao Carlos', 'Centro', 'Avenida Sao Carlos', '2271', 'Voltz', 'Comercial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13560321', 'Brasil', 'SP', 'Sao Paulo', 'Lapa', 'Rua das Flores', '2215', 'Lacoste', 'Comercial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13560123', 'Brasil', 'SP', 'Sao Carlos', 'Centro', 'Avenida de Bandis', '329', 'Juriti', 'Comercial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13560456', 'Brasil', 'SP', 'Sao Carlos', 'Centro', 'Avenida dos crias', '270', 'Palheiro', 'Comercial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13560789', 'Brasil', 'SP', 'Sao Carlos', 'Subsetor 7', 'Avenida NTP', '158', 'Cigarro de ferro', 'Comercial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13560963', 'Brasil', 'SP', 'Sao Carlos', 'Morumbi', 'Avenida Comercial', '785', 'concordo', 'Comercial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13560852', 'Brasil', 'SP', 'Sao Carlos', 'Centro', 'Avenida Getulio Vargas', '1500', 'barraca', 'Comercial');
+
+INSERT INTO barbearia.localizacao
+(cep, pais, estado, cidade, bairro, logradouro, numero, complemento, tipo_endereco)
+VALUES('13560741', 'Brasil', 'SP', 'Sao Carlos', 'Granada', 'Marechal Pinheros', '1563', 'essencia', 'Comercial');
+
+
+
+-- Inserindo departamentos
+INSERT INTO barbearia.departamento
+(nome, salario_fixo)
+VALUES('Corte Masculino', 2000.00);
+
+INSERT INTO barbearia.departamento
+(nome, salario_fixo)
+VALUES('Corte Feminino', 2000.00);
+
+INSERT INTO barbearia.departamento
+(nome, salario_fixo)
+VALUES('Manicure e Pedicure', 3000.00);
+
+INSERT INTO barbearia.departamento
+(nome, salario_fixo)
+VALUES('Depilação', 1500.00);
+
+
+-- Inserindo profissionais
+INSERT INTO barbearia.profissional
+(data_de_admissao, nome, email, cpfoucnpj, departamento_iddepartamento, localizacao_idendereco)
+VALUES('2022/12/20', 'Lucas Carlos', '987654321', 'lucascarlos@email.com', 1, 2);
+
+INSERT INTO barbearia.profissional
+(data_de_admissao, nome, email, cpfoucnpj, departamento_iddepartamento, localizacao_idendereco)
+VALUES('2022/08/10', 'Brad Pitt', '987654111', 'BradPitt@email.com', 1, 17),('2022/05/21', 'Bruna Marquezine', '987654222', 'BrunaMarquezine@email.com', 3, 18),
+('2022/03/20', 'Paolla de Oliveira', '987654333', 'PaollaOliveira@email.com', 5, 19),('2022/06/13', 'Marjorie Dias', '98765444', 'MarjorieDias@email.com', 1, 10),
+('2022/01/14', 'Roberto de Souza', '987654555', 'RobertoSouza@email.com', 5, 9),('2022/07/18', 'Larissa de Macedo', '987654666', 'LarissaMacedo@email.com', 2, 8),
+('2022/11/19', 'Agenor', '987654777', 'Agenor@email.com', 2, 19),('2022/11/19', 'Glória Menezes', '987654888', 'GlóriaMenezes@email.com', 1, 18),
+('2022/03/02', 'Maria Fonseca', '987654999', 'MariaFonseca@email.com', 3, 17);
+
+
+-- Inserindo estoques
+INSERT INTO barbearia.estoque
+(data_de_entrada, quantidade, validade, lote, data_de_fabricacao)
+VALUES('2022/10/12', '50', '2023/12/30', '1', '2022/08/02');
+
+
+INSERT INTO barbearia.estoque
+(data_de_entrada, quantidade, validade, lote, data_de_fabricacao)
+VALUES('2022-08-12', '25', '2024-12-30', '1', '2022/08/10');
+
+
+INSERT INTO barbearia.estoque
+(data_de_entrada, quantidade, validade, lote, data_de_fabricacao)
+VALUES('2022-07-12', '30', '2025-12-22', '1', '2022/12/10');
+
+
+INSERT INTO barbearia.estoque
+(data_de_entrada, quantidade, validade, lote, data_de_fabricacao)
+VALUES('2022-09-12', '89', '2023-12-30', '1', '2021/12/30');
+
+INSERT INTO barbearia.estoque
+(data_de_entrada, quantidade, validade, lote, data_de_fabricacao)
+VALUES('2022-04-10', '20', '2030-04-10', '1', '2022-02-09'),
+('2022-04-10', '25', '2023-04-10', '2', '2022-03-01'),
+('2022-12-03', '10', '2024-12-03', '1', '2022-11-10'),
+('2022-10-10', '15', '2024-04-10', '2', '2022-04-10'),
+('2022-06-18', '25', '2023-05-13', '1', '2022-05-13'),
+('2022-03-09', '30', '2024-01-15', '3', '2022-01-15');
+
+
+
+
+-- Inserindo produtos
+INSERT INTO barbearia.produto
+(nome, preco_de_venda, preco_de_custo, estoque_idestoque)
+VALUES('Shampoo', '35.90', '28.90', 1);
+
+
+INSERT INTO barbearia.produto
+(nome, preco_de_venda, preco_de_custo, estoque_idestoque)
+VALUES('Heineken LongNeck', '9.5', '6.80', 2);
+
+
+INSERT INTO barbearia.produto
+(nome, preco_de_venda, preco_de_custo, estoque_idestoque)
+VALUES('Gel de Cabelo', '20.5', '12.7', 3);
+
+
+INSERT INTO barbearia.produto
+(nome, preco_de_venda, preco_de_custo, estoque_idestoque)
+VALUES('Creme Nivea', '25.5', '18.7', 4);
+
+INSERT INTO barbearia.produto
+(nome, preco_de_venda, preco_de_custo, estoque_idestoque)
+VALUES('cigarro', 10, 3.50, 5),
+('chocolate', 5.50, 2.50, 6),
+('Spray pré-escova', 35, 20, 7),
+('Condicionador', 25.50, 15.50, 8),
+('esmalte',10, 8.50, 9),
+('Mascara Hidratante', 40, 30.50, 10);
+
+
+-- Inserindo fornecedores
+INSERT INTO barbearia.fornecedor
+(cnpj, nome, localizacao_idendereco)
+VALUES('9458348957', 'Hariel Costa', 3);
+
+
+INSERT INTO barbearia.fornecedor
+(cnpj, nome, localizacao_idendereco)
+VALUES('553499187622', 'Maria Abel', 5);
+
+INSERT INTO barbearia.fornecedor
+(cnpj, nome, localizacao_idendereco)
+VALUES('9458348957', 'Ambev', 19),('9458348958', 'Jhonsons e Jhonsons', 20),('9451348957', 'Paulistinha', 22),('9411148957', 'Cássio Ramos', 23),
+('9458222957', 'Mabel', 24),('9458343337', 'Toffe', 25),('4448348957', 'Frigels', 26),('5558348957', 'Elma Chips', 27),('668348957', 'Gusttavo Lima', 28);
+
+
+-- Inserindo relação entre produto e fornecedor
+INSERT INTO barbearia.produto_has_fornecedor  (produto_idproduto,fornecedor_idfornecedor)
+VALUES
+  (3,5),
+  (7,5),
+  (3,12),
+  (10,4),
+  (1,4),
+  (9,10),
+  (7,2),
+  (2,6),
+  (9,9),
+  (2,4),
+  (9,5),
+  (9,12),
+  (11,8),
+  (7,6),
+  (6,6),
+  (4,6),
+  (2,11),
+  (6,12),
+  (8,13),
+  (4,13);
+
+
+-- Inserindo relação entre produto e servico
+INSERT INTO barbearia.produto_has_servico  (produto_idproduto,servico_idservico)
+VALUES
+  (8,5),
+  (3,8),
+  (10,10),
+  (7,4),
+  (4,10),
+  (6,9),
+  (9,9),
+  (8,7),
+  (6,10),
+  (1,6),
+  (4,3),
+  (10,4),
+  (3,6),
+  (4,9),
+  (4,2),
+  (1,7),
+  (11,11),
+  (2,4),
+  (3,4),
+  (2,11);
+ 
+
+
+-- Inserindo relação entre produto e servico
+INSERT INTO barbearia.servico_has_atendimento  (servico_idservico,atendimento_idatendimento ,profissional_idmatricula)
+VALUES
+  (6,16,18),
+  (4,6,20),
+  (4,5,15),
+  (4,21,16),
+  (5,17,19),
+  (5,10,16),
+  (3,18,13),
+  (10,15,16),
+  (6,22,18),
+  (3,23,18),
+  (7,10,19),
+  (2,19,15),
+  (9,9,18),
+  (10,7,12),
+  (4,18,14),
+  (2,19,13),
+  (2,15,16),
+  (6,15,19),
+  (7,9,16),
+  (10,11,20);
+
+-- Inserindo atendimentos
+INSERT INTO barbearia.atendimento
+(confirmadooucancelado, data_solicitacao, tempo_de_atendimento, data_agendamento, preco_total, inicio_do_atendimento, final_do_atendimento, cliente_idmatricula, forma_de_pagamento, desconto)
+VALUES(true, '2022-12-22', 65, '2022-11-22', 35.90, '2022-12-22 15:00', '2022-12-22 16:00', 2, 'Cartão de Crédito', 0);
+
+INSERT INTO barbearia.atendimento
+(confirmadooucancelado, data_solicitacao, tempo_de_atendimento, data_agendamento, preco_total, inicio_do_atendimento, final_do_atendimento, cliente_idmatricula, forma_de_pagamento, desconto)
+VALUES(true, '2022-12-14', 40, '2022-12-22', 30.90, '2022-12-22 12:00', '2022-12-22 13:00', 2, 'Cartão de Débito', 0);
+
+INSERT INTO barbearia.atendimento
+(confirmadooucancelado, data_solicitacao, tempo_de_atendimento, data_agendamento, preco_total, inicio_do_atendimento, final_do_atendimento, cliente_idmatricula, forma_de_pagamento, desconto),
+VALUES(true,'2022-12-01',30,'2022-12-01',30,'2022-12-01 10:00','2022-12-01 10:30',3,'PIX',0),
+(true,'2022-12-02',31,'2022-12-02',40,'2022-12-02 10:00','2022-12-02 10:30',2,'Cartão de Credito',0),
+(false,'2022-12-03',32,'2022-12-03',50,'2022-12-03 10:00','2022-12-03 10:30',3,'Debito',0),
+(true,'2022-12-04',33,'2022-12-04',60,'2022-12-04 10:00','2022-12-04 10:30',4,'Dinheiro',0),
+(true,'2022-12-05',34,'2022-12-05',70,'2022-12-05 10:00','2022-12-05 10:30',5,'PIX',0),
+(false,'2022-12-06',35,'2022-12-06',30,'2022-12-06 10:00','2022-12-06 10:30',6,'Cartão de Credito',0),
+(true,'2022-12-07',36,'2022-12-07',40,'2022-12-07 10:00','2022-12-07 10:30',7,'Debito',0),
+(true,'2022-12-08',37,'2022-12-08',50,'2022-12-08 10:00','2022-12-08 10:30',8,'Dinheiro',0),
+(false,'2022-12-09',38,'2022-12-09',60,'2022-12-09 10:00','2022-12-09 10:30',9,'PIX',0),
+(true,'2022-12-10',39,'2022-12-10',70,'2022-12-10 10:00','2022-12-10 10:30',10,'Dinheiro',0);
+
+-- Inserindo Ordem de Compra
+INSERT INTO barbearia.ordem_de_compra
+(data_de_compra, preco_total, fornecedor_idfornecedor, departamento_iddepartamento)
+VALUES('2022-12-13', 890.50, 5, 1),
+		('2022-12-30', 90.50, 7, 2),
+		('2022-10-02', 1050.50, 8, 1),
+	('2022-11-05', 789.50, 9, 2),
+	('2022-10-28', 675.50, 10, 3),
+	('2022-12-01', 227.50, 1, 5),
+	('2022-01-30', 1765.50, 2,5),
+	('2022-06-25', 900.50, 2, 5),
+	('2022-02-04', 876.50, 12, 2),
+	('2022-06-25', 900.50, 11, 1);
+
+--Inserindo Item de Compra
+
+INSERT INTO barbearia.item_compra
+(quantidade, valor_item, preco_total, produto_idproduto, idordem_de_compra)
+VALUES(2, 9.90, 18.80, 1,12),
+(1, 9.90, 9.90, 6,13),
+(3, 15.00, 45.00, 3,14),
+(2, 9.90, 18.80, 4,15),
+(5, 15.00, 75.00, 11,16),
+(2, 9.90, 18.80, 6,17),
+(3, 9.90, 27.70, 7,18),
+(2, 15.00, 30.00, 8,19),
+(1, 9.90, 9.90, 9,20),
+(1, 15.00, 15.00, 10,21),
+(2, 10.90, 21.80, 4,1);
+
+
+--Inserindo Item de Venda
+INSERT INTO barbearia.item_venda
+(quantidade, preco_unitario, atendimento_idatendimento, produto_idproduto)
+VALUES(1, 35.90, 2, 1);
+
+INSERT INTO barbearia.item_venda
+(quantidade, preco_unitario, atendimento_idatendimento, produto_idproduto)
+VALUES(2, 9.90, 1, 1),
+(1, 09.90, 2, 2),
+(3, 19.90, 3, 3),
+(4, 05.90, 4, 4),
+(1, 29.90, 5, 5),
+(3, 08.90, 6, 6),
+(2, 06.90, 7, 7),
+(4, 04.90, 8, 8),
+(2, 11.90, 2, 9),
+(2, 13.90, 9, 6);
+
+--Inserindo Serviço
+INSERT INTO barbearia.servico
+(nome, preco, tempo_de_atendimento, departamento_iddepartamento)
+VALUES('Corte Masculino + Barba', 35.90, 50, 1),
+('Depilação a laser', 600.90, 120, 5),
+('Corte Feminino + Penteado', 55.90, 180, 2),
+('Manicure', 25.90, 60, 3),
+('Manicure + Pedicure', 60.90, 90, 3),
+('Corte Masculino', 25.90, 60, 1),
+('Barba', 35.90, 50, 1),
+('Sobrancelha', 15.90, 50, 2),
+('Barba + Corte + Nevou', 157.90, 50, 1),
+('Corte Blindado + Bigode fino', 55.90, 50, 1);
+
+--Inserindo telefone
+INSERT INTO barbearia.telefone (ddd,numero ,profissional_idmatricula,cliente_idmatricula,fornecedor_idfornecedor,tipo_telefone)
+VALUES (49,'6291-5306',1,NULL,NULL,'Residencial'),
+  (51,'1666-7318',12,NULL,NULL,'Residencial'),
+  (34,'8078-9533',13,NULL,NULL,'Comercial'),
+  (26,'7384-7779',14,NULL,NULL,'Comercial'),
+  (10,'8477-3329',15,NULL,NULL,'Celular'),
+  (51,'1393-3629',16,NULL,NULL,'Celular'),
+  (55,'9346-7758',17,NULL,NULL,'Residencial'),
+  (21,'9275-3628',18,NULL,NULL,'Residencial'),
+  (47,'1248-6466',19,NULL,NULL,'Comercial'),
+  (41,'5015-2460',20,NULL,NULL,'Comercial'),
+  (67,'2218-9436',NULL,NULL,1,'Celular'),
+  (22,'2881-4622',NULL,NULL,2,'Celular'),
+  (42,'2268-3376',NULL,NULL,3,'Residencial'),
+  (60,'1163-5826',NULL,NULL,4,'Residencial'),
+  (50,'9521-4773',NULL,NULL,5,'Comercial'),
+  (23,'2309-9869',NULL,NULL,6,'Comercial'),
+  (21,'6724-7306',NULL,NULL,7,'Celular'),
+  (65,'1083-4385',NULL,NULL,8,'Celular'),
+  (67,'4801-1178',NULL,NULL,9,'Residencial'),
+  (47,'9244-8678',NULL,NULL,10,'Residencial'),
+  (61,'4864-4782',NULL,2,NULL,'Residencial'),
+  (13,'3415-2071',NULL,3,NULL,'Residencial'),
+  (43,'6562-1275',NULL,4,NULL,'Comercial'),
+  (66,'9433-0788',NULL,5,NULL,'Comercial'),
+  (19,'6336-6319',NULL,6,NULL,'Celular'),
+  (63,'9663-8134',NULL,7,NULL,'Celular'),
+  (49,'1057-7805',NULL,8,NULL,'Residencial'),
+  (46,'3556-6175',NULL,9,NULL,'Residencial'),
+  (19,'2572-3477',NULL,10,NULL,'Comercial'),
+  (56,'4564-1024',NULL,11,NULL,'Comercial');
+ 
+ 
+ 
+ INSERT INTO barbearia.telefone (ddd,numero ,profissional_idmatricula,cliente_idmatricula,fornecedor_idfornecedor,tipo_telefone)
+VALUES (49,"6291-5306",1,NULL,NULL,"Residencial");
+ 
